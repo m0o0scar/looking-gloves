c1d75d8fd09eb143adb8ccabb55c685c43fccd03
Oscar <moscartong@gmail.com>
2022-12-21 05:10:40 +0800

    feat: determine frames direction; remove light field export option, keep only the quilt image

--- utils/video.ts
+++ utils/video.ts
@@ -38,3 +38,30 @@ export const imagesToVideo = (images: HTMLCanvasElement[]) => {
     drawFrame();
   });
 };
+
+const calculateColorDifference = (
+  frame1: HTMLCanvasElement,
+  frame2: HTMLCanvasElement,
+  offset: number
+) => {
+  const ctx1 = frame1.getContext('2d')!;
+  const ctx2 = frame2.getContext('2d')!;
+  const imageData1 = ctx1.getImageData(0, 0, frame1.width - offset, frame1.height);
+  const imageData2 = ctx2.getImageData(offset, 0, frame2.width - offset, frame2.height);
+  const data1 = imageData1.data;
+  const data2 = imageData2.data;
+  let difference = 0;
+  for (let i = 0; i < data1.length; i += 4) {
+    difference += Math.abs(data1[i] - data2[i]);
+    difference += Math.abs(data1[i + 1] - data2[i + 1]);
+    difference += Math.abs(data1[i + 2] - data2[i + 2]);
+  }
+  return difference;
+};
+
+export const framesAreLeftToRight = (frames: HTMLCanvasElement[]) => {
+  // TODO maybe scale down the frames (to 100px x 100px?) to speed up the calculation?
+  const diff0 = calculateColorDifference(frames[0], frames[1], 0);
+  const diff1 = calculateColorDifference(frames[0], frames[1], 1);
+  return diff0 < diff1;
+};
