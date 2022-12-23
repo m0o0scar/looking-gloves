f5ae0f305008ad55859318b5bafaccf24ffe8063
Oscar <moscartong@gmail.com>
2022-12-23 21:27:48 +0800

    chore: make home button always visible

--- components/common/HomeBtn.tsx
+++ components/common/HomeBtn.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react';
 export const HomeBtn: FC = () => {
   return (
     <Link href="/">
-      <button className="btn btn-sm btn-ghost btn-square absolute top-2 right-2">
+      <button className="btn btn-sm btn-ghost btn-square fixed top-2 right-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
