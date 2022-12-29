5603022f361fb5112dec17e493328ab60a73ec9c
Oscar <tangqh@sea.com>
2022-12-29 21:43:47 +0800

    fix(ui): video/images page responsive design

--- components/processors/ImageSequenceExtractor.tsx
+++ components/processors/ImageSequenceExtractor.tsx
@@ -151,13 +151,30 @@ export const ImageSequenceExtractor: SequenceProcessorInfo = ({
     <>
       <h2>Please select video or light field photoset</h2>
 
-      <p>
-        For Light Field photoset, please select{' '}
-        <mark>
-          <b>ALL</b>
-        </mark>{' '}
-        the image files.
-      </p>
+      <div className="alert alert-info shadow-lg max-w-xl">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-current flex-shrink-0 w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>
+            For Light Field photoset, please select{' '}
+            <mark>
+              <b>ALL</b>
+            </mark>{' '}
+            the image files.
+          </span>
+        </div>
+      </div>
 
       {/* file input for selecting video file */}
       <input
@@ -165,7 +182,7 @@ export const ImageSequenceExtractor: SequenceProcessorInfo = ({
         type="file"
         accept="video/*, image/*"
         multiple
-        className="file-input h-auto"
+        className="file-input h-auto w-[36rem] max-w-full mt-2"
         onChange={(e) => setFiles(e.target.files)}
       />
 
