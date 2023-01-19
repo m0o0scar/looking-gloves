7717a1446af570a3a850ef80a5eaf7240b296009
Oscar <tangqh@sea.com>
2023-01-19 17:34:25 +0800

    fix(kaggle): prevent re-uploading previous outputs

--- api/kaggle/depthmap/status.py
+++ api/kaggle/depthmap/status.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import os
 import requests
+import time
 
 from ..share import get_root_folder, create_kaggle_client
 
@@ -16,18 +17,21 @@ def check_notebook_status():
 
   outputs = []
   if status['status'] == 'complete':
-    # download output
     root_folder = get_root_folder()
-    download_folder = os.path.join(root_folder, "downloads", notebook_id)
+    ts = str(round(time.time() * 1000))
+    download_folder = os.path.join(root_folder, "downloads", notebook_id, ts)
+    print('download folder = %s' % download_folder)
     kaggle.kernels_output(notebook_id, download_folder, force=True)
 
     # update all images file to a temporary online storage
     output_folder = os.path.join(download_folder, 'outputs')
     for file in os.listdir(output_folder):
-      if file.endswith('.png'):
+      if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg'):
         with open(os.path.join(output_folder, file), 'rb') as f:
           response = requests.post('https://tmpfiles.org/api/v1/upload', files={'file': f})
-          download_url = response.json()['data']['url']
+          result = response.json()
+          print(result)
+          download_url = result['data']['url']
           download_url = download_url.replace('https://tmpfiles.org/', 'https://tmpfiles.org/dl/')
           outputs.append(download_url)
 
