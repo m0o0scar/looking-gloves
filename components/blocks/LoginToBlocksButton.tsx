a75052bff0027731def9a96db3a057bbd4ef760f
Oscar <tangqh@sea.com>
2023-01-30 17:57:47 +0800

    feat: navigate to blocks when click on blocks button after logged in

--- components/blocks/LoginToBlocksButton.tsx
+++ components/blocks/LoginToBlocksButton.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import cls from 'classnames';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import useBlocksAPI from './useBlocksAPI';
 
 export const LoginToBlocksButton: FC = () => {
   const { loggedIn, login, me } = useBlocksAPI();
+  const [pending, setPending] = useState(false);
 
   let label = '';
   if (loggedIn === undefined) label = '...';
@@ -14,14 +15,30 @@ export const LoginToBlocksButton: FC = () => {
   else label = '...';
 
   const onClick = () => {
-    if (loggedIn === false) {
-      login();
+    switch (loggedIn) {
+      case false:
+        setPending(true);
+        login();
+        break;
+
+      case true:
+        window.open('https://blocks.glass/manage', '_blank');
+        break;
     }
   };
 
   return (
-    <button className={cls('btn btn-sm no-animation not-prose')} onClick={onClick}>
-      <img src="/assets/lookingglass-blocks.png" alt="Looking Glass Blocks" className="h-5 mr-1" />
+    <button
+      className={cls('btn btn-sm no-animation not-prose', { disabled: pending, loading: pending })}
+      onClick={onClick}
+    >
+      {!pending && (
+        <img
+          src="/assets/lookingglass-blocks.png"
+          alt="Looking Glass Blocks"
+          className="h-5 mr-1"
+        />
+      )}
       {label}
     </button>
   );
